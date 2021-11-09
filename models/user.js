6a8f3028ae2201ca3c52edffe5ac88201bf3fb9d
Sudip Thapa <sthapa@deerhold.org>
2021-11-09 08:09:55 +0545

    initial login structure

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    firstname: {
+        type: String,
+        trim: true,
+    },
+    lastname: {
+        type: String,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profilePic: {
+        type: String
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model('User', userSchema);
